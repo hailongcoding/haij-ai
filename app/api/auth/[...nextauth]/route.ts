234dbd7fbf0f8e3e3a351acb943d5f38c48dec31
hailongcoding <156517170+hailongcoding@users.noreply.github.com>
2026-02-20 22:24:10 -0600

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin', // optional custom page
+  },
+});
+
+export { handlers as GET, handlers as POST };
